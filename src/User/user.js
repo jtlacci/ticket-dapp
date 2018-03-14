bcc9988273a4dab9a9c6b5786748545f94d4ec9f
jtlacci <johntsterlacci@gmail.com>
2018-03-14 11:41:02 -0400

    added user tests

--- src/User/user.js
+++ src/User/user.js
@@ -1,48 +1,68 @@
 import web3 from '../Utils/web3'
 import firebaseRef from '../Utils/firebase'
 
-const CHANGE_INFO = 'user/CHANGE_INFO'
-const CHANGE = 'user/CHANGE'
-const GET = 'user/GET'
+const CREATE_ACCOUNT= 'user/CREATE_ACCOUNT'
+const UPDATE_INFO = 'user/UPDATE_INFO'
+const UPDATE = 'user/UPDATE'
+const TOGGLE_HAS_ACCOUNT = 'user/TOGGLE_HAS_ACCOUNT'
 
 
-const initState = {
-  currentUser: {}
+
+const initialState = {
+  currentUser: {},
+  currentInfo: {},
+  hasAccount: false,
 }
 
-export default function reducer(state = initState, action){
+export default function reducer(state = initialState, action){
   switch(action.type){
-    case CHANGE:
+    case UPDATE:
       return {...state, currentUser:action.user}
-    case CHANGE_INFO:
+    case UPDATE_INFO:
       return {...state, currentInfo:action.userInfo}
+    case TOGGLE_HAS_ACCOUNT:
+      let prevState = state.hasAccount
+      return {...state, hasAccount:!prevState}
     default: return state;
   }
 }
 
-// create user
 
-// change user
-export function changeUser(user){
-  return{ type: CHANGE , user}
+// UPDATE user
+export function updateUser(user){
+  return{ type: UPDATE , user}
 }
 
-export function changeUserInfo(user, userInfo){
-  return{ type: CHANGE_INFO , user, userInfo}
+export function updateUserInfo(user, userInfo){
+  return{ type: UPDATE_INFO , user, userInfo}
+}
+
+export function toggleHasAccount(){
+  return{ type: TOGGLE_HAS_ACCOUNT}
 }
 
 // get user's events
 
 // side effects
+
 // get user from web3
 export function getUser(){
   return async (dispatch) => {
     var user = await web3.eth.accounts[0]
-    dispatch(changeUser(user))
+    dispatch(updateUser(user))
     if(user !== {}){
       dispatch(lookupUser(user))
     }
   }
+}
+// create account
+export function createAccount(_user,_info){
+  return async (dispatch) => {
+  await firebaseRef.child('user/'+_user).set(_info)
+  dispatch(updateUserInfo(_user,_info))
+  dispatch(toggleHasAccount())
+  }
+
 }
 // lookup user from database
 export function lookupUser(_web3user){
@@ -50,6 +70,8 @@ export function lookupUser(_web3user){
     var snapshot = await firebaseRef.child('user/'+_web3user).once('value')
     var value = snapshot.val()||{}
     if(value.length > 0){
-      dispatch(changeUserInfo(_web3user,value))}
+      dispatch(updateUserInfo(_web3user,value))
+      dispatch(toggleHasAccount())
     }
+  }
 }
---
+++ src/User/user.test.js
@@ -0,0 +1,67 @@
+import reducer from './user'
+import * as actions from './user'
+
+const CREATE_ACCOUNT= 'user/CREATE_ACCOUNT'
+const UPDATE_INFO = 'user/UPDATE_INFO'
+const UPDATE = 'user/UPDATE'
+const TOGGLE_HAS_ACCOUNT = 'user/TOGGLE_HAS_ACCOUNT'
+
+
+describe('user', () => {
+  const initialState = {
+    currentUser: {},
+    currentInfo: {},
+    hasAccount: false,
+  }
+
+  it('should have initial state', () =>{
+    expect(
+      reducer(undefined,{type:'empty'})
+    ).toEqual(
+      initialState
+    )
+  })
+
+  var updateUserState = {
+    ...initialState,
+    currentUser:'testUser'}
+
+  it('should update a new user', () =>{
+    expect(
+      reducer(undefined, {
+        type: UPDATE,
+        user: 'testUser'
+      })
+    ).toEqual(updateUserState)
+  })
+
+  var updateUserInfo ={
+    ...updateUserState,
+    currentInfo:{
+      info:'testInfo'
+    }}
+
+  it('should update user info', () =>{
+    expect(
+      reducer(updateUserState, {
+        type: UPDATE_INFO,
+        userInfo: {info:'testInfo'}
+      })
+    ).toEqual(
+      updateUserInfo
+    )
+  })
+
+  it('should toggle has account', () =>{
+    expect(
+      reducer(undefined, {
+        type: TOGGLE_HAS_ACCOUNT,
+      })
+    ).toEqual(
+      {...initialState,
+        hasAccount:true
+      }
+    )
+  })
+
+})
