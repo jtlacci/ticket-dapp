80f69f36b38c3e5c5addacd1888b9e2c50160558
jtlacci <johntsterlacci@gmail.com>
2018-03-14 23:35:25 -0400

    added event side effects

--- src/Events/events.js
+++ src/Events/events.js
@@ -1,6 +1,50 @@
+import web3 from '../Utils/web3'
+import firebaseRef from '../Utils/firebase'
 
-// create new event
+var _ = require('lodash');
 
-// cancel event
 
-// destroy event 
+export default function reducer(state = {}, action){
+  switch(action.type){
+    default:
+      return state
+  }
+}
+
+// add event to list
+
+
+//look up event info
+
+//look up user in event log
+
+
+
+// side effects
+
+// add event to database
+export function addEventToDB(_eventAddress){
+  return async (dispatch) => {
+    await firebaseRef.child('event/').push(_eventAddress)
+
+    //TODO: Add event action
+
+  }
+}
+
+// get events from database
+export function lookupEvents(){
+  return async (dispatch) => {
+    var snapshot = await firebaseRef.child('event').once('value')
+    var value = snapshot.val()||{}
+    if(!_.isEmpty(value)){
+
+      //TODO: add event action
+
+    }
+  }
+}
+
+// get event list from database
+
+//get log of event
