325d0bbb96d52c48936b15a9825100e6113cdf53
jtlacci <johntsterlacci@gmail.com>
2018-03-15 14:57:17 -0400

    event contract tests

--- test/events.js
+++ test/events.js
@@ -6,7 +6,12 @@ contract('Events', function(accounts) {
 
   it("should get event info", async function() {
     let instance = await Events.new('test',10,1,10)
+    //get event name
     assert.equal(await instance.name.call(),'test')
+    //get event seats
+    assert.equal(await instance.seats.call(),10)
+    //get price
+    assert.equal(await instance.price.call(),1)
   });
 
 });
